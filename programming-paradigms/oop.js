64539cb9a5d595b0d8899bb669f3bb38f476b1b2
jym <jiangyuming@hoge.cn>
2022-09-06 18:02:32 +0800

    命令式编程和面向对象编程demo实践

---
+++ programming-paradigms/oop.js
@@ -0,0 +1,58 @@
+// 面向对象编程，前几年比较火，通过类或者对象的方式来组织代码逻辑(属性+方法)
+document.getElementsByClassName('main-title')[0].innerText = '面向对象编程'
+
+// 校验
+class Validator {
+    static REQUIRED = 'REQUIRED';
+    static MIN_LENGTH = 'MIN_LENGTH';
+    static validatorHandler(value, flag, validatorValue) {
+        if (flag === this.REQUIRED) {
+            return value.trim().length > 0
+        }
+        if (flag === this.MIN_LENGTH) {
+            return value.trim().length > validatorValue
+        }
+        return false
+    }
+    
+}
+
+// 用户
+class User {
+    constructor(uname, upass) {
+        this.userName = uname
+        this.userPassword = upass
+    }
+    sayHi() {
+        const user = {
+            userName: this.userName,
+            userPass: this.userPassword
+        }
+        console.log(user)
+        console.log('hi, im' + user.userName)
+    }
+}
+
+// 用户相关的数据表单
+class UserForm {
+    constructor() {
+        this.userName = document.getElementById('username')
+        this.password = document.getElementById('password')
+        this.form = document.getElementById('input-form')
+        this.form.addEventListener('submit', this.getUserInfo.bind(this))
+    }
+    getUserInfo(e) {
+        e.preventDefault();
+        const userValue = this.userName.value;
+        const passwordValue = this.password.value;
+        // 静态属性方法可以直接使用类名来调用
+        if (!Validator.validatorHandler(userValue, Validator.REQUIRED) || !Validator.validatorHandler(passwordValue, Validator.MIN_LENGTH, 5)) {
+            alert('invalid input! userName or password is wrong (the password should be at least 6 characters)!')
+            return
+        }
+
+        const user = new User(userValue, passwordValue)
+        user.sayHi()
+    }
+}
+new UserForm()
--- programming-paradigms/procedual.js
+++ programming-paradigms/procedual.js
@@ -1,1 +1,29 @@
+// 命令式编程，语法简单，复用性低，注重过程的呈现，命令的罗列
 document.getElementsByClassName('main-title')[0].innerText = '命令式编程'
+
+
+const form = document.getElementById('input-form')
+function showUserInfo(e) {
+    // 提交后阻止外链跳转
+    e.preventDefault();
+    const userName = document.getElementById('username')
+    const userNameValue = userName.value
+    const password = document.getElementById('password')
+    const passwordValue = password.value
+    if (userNameValue.trim().length === 0) {
+        alert('invalid input! Username must not be empty')
+        return
+    }
+    if (passwordValue.trim().length < 6) {
+        alert('invalid input! password must be 6 characters or longer')
+        return
+    }
+    const user = {
+        name: userNameValue,
+        password: passwordValue
+    }
+    console.log(user)
+    console.log("HI, I'M" + user.name)
+}
+
+form.addEventListener('submit', showUserInfo)
